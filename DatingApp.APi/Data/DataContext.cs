669cdbb15ee039f5bafeea198a618817f9ec0201
Armando <armandoneto@Armandos-iMac.local>
2019-02-03 17:57:30 -0500

    Initial Commit

---
+++ DatingApp.APi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.APi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.APi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
